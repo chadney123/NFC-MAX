2f842b8c612b57fe08a6816c02b5e19c7c0e2065
chadney123 <chadney123@yahoo.com>
2026-02-04 08:22:59 +0000

    Modify service worker to skip waiting on install

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  // This allows the app to work while the service worker is active
   e.respondWith(fetch(e.request));
 });
