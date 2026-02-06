9eca1f510a5462a55fb0ba7cc1a7abacd1d61d19
chadney123 <chadney123@yahoo.com>
2026-02-06 08:36:17 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,6 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request));
 });
