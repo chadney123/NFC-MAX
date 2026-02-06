8be105ad3fb9f078668c97807f29707e9d300bcf
chadney123 <chadney123@yahoo.com>
2026-02-06 08:26:36 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE_NAME = 'tracker-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './manifest.json', './logo.png'])));
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
